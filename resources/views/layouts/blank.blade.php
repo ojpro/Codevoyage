eb5e7e6285f69bb905650c67fafa4f1794d2f79f
ojpro <contact@ojpro.me>
2023-04-09 16:44:18 +0000

    update title

--- resources/views/layouts/blank.blade.php
+++ resources/views/layouts/blank.blade.php
@@ -4,7 +4,8 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title>Laravel</title>
+    // TODO: make it accessible
+    <title>{{ config('app.name') }}</title>
 
     @vite('resources/css/app.css')
 
