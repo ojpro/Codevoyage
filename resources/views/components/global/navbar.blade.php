a037e7db01845379e764e6c1196bbdc0147bdaf0
ojpro <contact@ojpro.me>
2023-04-09 20:31:04 +0000

    make the navbar works on small devices

--- resources/views/components/global/navbar.blade.php
+++ resources/views/components/global/navbar.blade.php
@@ -1,5 +1,5 @@
 <header>
-    <nav class="bg-white border-gray-200 px-4 lg:px-6 py-2.5 dark:bg-gray-800">
+    <nav class="bg-white border-gray-200 px-4 lg:px-6 py-2.5 dark:bg-gray-800" x-data="{ open: false }">
         <div class="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl">
             <a href="{{ route('home') }}">
                 <span class="self-center text-xl font-semibold whitespace-nowrap dark:text-white">Codevoyage</span>
@@ -22,7 +22,7 @@
                        class="text-gray-800 dark:text-white hover:bg-gray-50 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 dark:hover:bg-gray-700 focus:outline-none dark:focus:ring-gray-800">Log
                         in</a>
                 @endauth
-                <button data-collapse-toggle="mobile-menu-2" type="button"
+                <button type="button" @click="open = !open"
                         class="inline-flex items-center p-2 ml-1 text-sm text-gray-500 rounded-lg lg:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
                         aria-controls="mobile-menu-2" aria-expanded="false">
                     <span class="sr-only">Open main menu</span>
@@ -40,7 +40,8 @@
                     </svg>
                 </button>
             </div>
-            <div class="hidden justify-between items-center w-full lg:flex lg:w-auto lg:order-1" id="mobile-menu-2">
+            <div class="justify-between items-center w-full lg:flex lg:w-auto lg:order-1"
+                 :class="{ 'hidden': ! open }">
                 <ul class="flex flex-col mt-4 font-medium lg:flex-row lg:space-x-8 lg:mt-0">
                     <li>
                         <a href="{{ route('home') }}"
